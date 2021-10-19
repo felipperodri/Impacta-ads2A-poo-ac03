87ed93ef9994a470c414ef3c780c8fae9e7ef469
felipperodri <felipperodri@outlook.com>
2021-10-19 13:12:15 -0300

    Removed e-mail

--- loja.py
+++ loja.py
@@ -1,7 +1,7 @@
 # Programação Orientada a Objetos
 # AC03 ADS-EaD - Implementação de classes, herança, polimorfismo e lançamento de exceções.
 #
-# Email Impacta: felippe.leite@aluno.faculdadeimpacta.com.br
+# Email Impacta: _________________@aluno.faculdadeimpacta.com.br
 
 
 class Produto:
